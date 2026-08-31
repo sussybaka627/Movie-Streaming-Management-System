8f05e22ba6777985344e70c44c4e5cd0aea5c45b
sussybaka627 <lekhahoang5@gmail.com>
2026-08-31 20:46:11 +0700

    feat: implement watchlist model and favorite movie features

--- src/controllers/WatchlistController.java
+++ src/controllers/WatchlistController.java
@@ -1,1 +1,55 @@
 package controllers;
+
+import models.datastructures.MyQueue;
+import models.datastructures.MyStack;
+import models.entities.Movie;
+
+public class WatchlistController {
+    private MyQueue<Movie> watchlist;
+    private MyStack<Movie> undoStack;
+
+    public WatchlistController() {
+        this.watchlist = new MyQueue<>();
+        this.undoStack = new MyStack<>();
+    }
+
+    public void addMovieToWatchlist(Movie movie) {
+        watchlist.enqueue(movie);
+    }
+
+    public Movie watchNext() {
+        if (watchlist.isEmpty()) {
+            return null;
+        }
+        Movie nextMovie = watchlist.dequeue();
+        undoStack.push(nextMovie);
+        return nextMovie;
+    }
+
+    public Movie undoLastWatch() {
+        if (undoStack.isEmpty()) {
+            return null;
+        }
+        Movie lastWatched = undoStack.pop();
+        watchlist.enqueue(lastWatched);
+        return lastWatched;
+    }
+
+    public void displayWatchlist() {
+        if (watchlist.isEmpty()) {
+            System.out.println("Watchlist is currently empty.");
+            return;
+        }
+        
+        System.out.println("\n--- Current Watchlist ---");
+        int count = 1;
+        int size = watchlist.size();
+        
+        for (int i = 0; i < size; i++) {
+            Movie m = watchlist.dequeue();
+            System.out.println(count++ + ". " + m.getTitle() + " (ID: " + m.getId() + ")");
+            watchlist.enqueue(m);
+        }
+        System.out.println("-------------------------");
+    }
+}
